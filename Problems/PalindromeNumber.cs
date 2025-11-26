38f76d2efd46879ec557753df8e3697d3f5954d4
Arjun Haridas <arjunharidas@carestack.com>
2025-11-27 01:20:02 +0530

    New changes

---
+++ Problems/PalindromeNumber.cs
@@ -0,0 +1,43 @@
+﻿using ProblemSolving.Abstractions;
+using ProblemSolving.Framework;
+
+namespace ProblemSolving.Problems;
+
+/// <summary>
+/// Given an integer x, return true if x is a palindrome, and false otherwise.
+/// <see cref="https://leetcode.com/problems/palindrome-number/"/>
+/// </summary>
+
+[ProblemId(9)]
+public class PalindromeNumber : IProblem
+{
+    private int _input;
+    private bool _isPalindrome;
+    
+    public PalindromeNumber(int input)
+    {
+        _input = input;
+    }
+    public void Execute()
+    {
+        if (_input < 0)
+        {
+            return;
+        }
+        int copy = _input;
+        int reverse = 0;
+        while (_input > 0)
+        {
+            int r = _input % 10;
+            reverse = reverse * 10 + r;
+            _input = _input / 10;
+        }
+        _isPalindrome = copy == reverse;
+    }
+
+    public void DisplayResult()
+    {
+        string text = _isPalindrome ? "palindrome" : "not palindrome";
+        Console.WriteLine($"The number is {text}");
+    }
+}
---
+++ Problems/ReverseInteger.cs
@@ -0,0 +1,66 @@
+﻿using ProblemSolving.Abstractions;
+using ProblemSolving.Framework;
+
+namespace ProblemSolving.Problems;
+
+/// <summary>
+/// Given a signed 32-bit integer x, return x with its digits reversed.
+/// If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1],
+/// then return 0.
+/// <see cref="https://leetcode.com/problems/reverse-integer/description/"/>
+/// </summary>
+
+[ProblemId(7)]
+public class ReverseInteger : IProblem
+{
+    private int _input;
+    private int _output;
+
+    public ReverseInteger(int intput)
+    {
+        _input = intput;
+    }
+    
+    public void Execute()
+    {
+        bool isNegative = _input < 0;
+        string maxIntString = isNegative ? int.MinValue.ToString().Substring(1) : int.MaxValue.ToString();
+        string inputIntString = isNegative ? _input.ToString().Substring(1) : Math.Abs(_input).ToString();
+        int inputIntLength = inputIntString.Length;
+        if (inputIntLength < maxIntString.Length)
+        {
+            for (int i = 0; i < inputIntLength; i++)
+            {
+                int value = inputIntString[inputIntLength - i - 1] - '0';
+                _output = _output * 10 + (isNegative ? -1 * value : value);
+            }
+        }
+        else
+        {
+            bool flag = false;
+            for (int i = 0; i < inputIntLength; i++)
+            {
+                if (inputIntString[inputIntLength - i - 1] <= maxIntString[i] || flag)
+                {
+                    int value = inputIntString[inputIntLength - i - 1] - '0';
+                    _output = _output * 10 + (isNegative ? -1 * value : value);
+                    if (inputIntString[inputIntLength - i - 1] < maxIntString[i])
+                    {
+                        flag = true;
+                    }
+                }
+                else
+                {
+                    _output = 0;
+                    break;
+                }
+            }
+        }
+    }
+
+    public void DisplayResult()
+    {
+        Console.WriteLine($"The reversed integer is {_output}");
+    }
+    
+}
